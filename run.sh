c596b181c76c01f65acf4a52a4054be5abf7f895
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-05 01:04:41 +0200

    Made run.sh executable

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python manage.py migrate --noinput
 # python manage.py collectstatic --noinput
 # python manage.py runserver "$VCAP_APP_HOST:$VCAP_APP_PORT"
 
-gunicorn opencodereview.wsgi --workers 2 --bind "$VCAP_APP_HOST:$VCAP_APP_PORT"
+gunicorn opencodereview.wsgi --workers 2 --bind "0.0.0.0:$PORT"
